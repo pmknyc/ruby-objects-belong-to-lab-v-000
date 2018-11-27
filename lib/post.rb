466cd44ac99d685d7e31055d8ef980d07fb6dfe7
Paula McKinley <pmknyc@optimum.net>
2018-11-27 21:04:09 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,5 @@
-# Posts should have a title 
-# and belong to an author. 
+# Posts should have a title
+# and belong to an author.
 # can tell you the name of its author
 # post.author.name
 class Post
